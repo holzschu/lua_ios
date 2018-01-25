30fb2bfb4d984fbcb9720e1279f266e753152a75
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2018-01-25 11:46:19 +0100

    Lua compatible with streams and pipes

--- get_lua_source.sh
+++ get_lua_source.sh
@@ -10,4 +10,9 @@ rm $LUAVERSION.tar.gz
 patch $LUAVERSION/src/lua.c < patch_luac.txt
 patch $LUAVERSION/src/luac.c < patch_luacc.txt
 patch $LUAVERSION/src/luaconf.h < patch_luaconf.txt
+patch $LUAVERSION/src/lauxlib.c < patch_lauxlibc.txt
+patch $LUAVERSION/src/ldblib.c < patch_ldblibc.txt
+patch $LUAVERSION/src/liolib.c < patch_liolibc.txt
+patch $LUAVERSION/src/loslib.c < patch_loslibc.txt
+
 
