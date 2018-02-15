2b22a0058aa2c0b94a166f6beede433b96f4e8ec
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2018-02-15 14:12:29 +0100

    Download ios_error.h for quasi-autonomous compilation

--- get_lua_source.sh
+++ get_lua_source.sh
@@ -10,6 +10,8 @@ rm $LUAVERSION.tar.gz
 echo "Downloading ios_system.framework"
 curl -OL $HHROOT/ios_system/releases/download/v$IOS_SYSTEM_VER/smallRelease.tar.gz
 ( tar -xzf smallRelease.tar.gz --strip 1 && rm smallRelease.tar.gz ) || { echo "ios_system failed to download"; exit 1; }
+echo "Downloading header file:"
+curl -OL $HHROOT/ios_system/releases/download/v$IOS_SYSTEM_VER/ios_error.h 
 
 
 # Patch sources:
