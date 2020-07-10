1ecccf314b2a615e17c7a0dcbba5fb5812d05506
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2020-07-10 21:45:52 +0200

    Updated Xcode project

--- compile_lua.sh
+++ compile_lua.sh
@@ -17,6 +17,7 @@ do
    rm -rf $framework.xcframework
    xcodebuild -create-xcframework -framework build/Release-iphoneos/$framework.framework -framework build/Release-iphonesimulator/$framework.framework -output $framework.xcframework
    # while we're at it, let's compute the checksum:
+   rm -f $framework.xcframework.zip
    zip -r $framework.xcframework.zip $framework.xcframework
    swift package compute-checksum $framework.xcframework.zip
 done
