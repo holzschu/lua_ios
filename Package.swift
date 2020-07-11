ffca90f3a2fbc712ced5db851e79f7a41fb38a6f
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2020-07-11 08:04:37 +0200

    Edited xcframework

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .binaryTarget(
             name: "lua_ios",
             url: "https://github.com/holzschu/lua_ios/releases/download/1.0/lua_ios.xcframework.zip",
-            checksum: "c329a787015d1fe1f1e6bef5c6f25ac8f72553885c53d07c3a93a05064b2fe59"
+            checksum: "6a42e114a9d49cf71f0c6e18bfa9b4dc5b9dbdbe67506c2b5ade16cfb4aca4cf"
         )
     ]
 )
