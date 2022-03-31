cb5c55d78afabc17277ff297368531957b296480
momoterraw <andrewvmail@gmail.com>
2022-03-31 12:57:30 -0700

    can compile without any errors

--- xcfs/Package.swift
+++ xcfs/Package.swift
@@ -11,8 +11,8 @@ _ = Package(
     targets: [
         .binaryTarget(
             name: "ios_system",
-            url: "https://github.com/holzschu/ios_system/releases/download/v2.7.0/ios_system.xcframework.zip",
-            checksum: "e98c075c088f916649426720afa50df03904aa36d321fe072c9bd6ccbc12806c"
+            url: "https://github.com/holzschu/ios_system/releases/download/v3.0.0/ios_system.xcframework.zip",
+            checksum: "66f4a5e696e29f3c70e8bab3a3e8c5f5a0bda48ab102e9db98a90db93b0981ea"
         ),
         .target(
             name: "build",
